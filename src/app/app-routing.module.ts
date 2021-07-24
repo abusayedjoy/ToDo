3639e6b3118617f6279fafb93172e257360aa6d9
abusayedjoy <sayed2912@gmail.com>
2021-07-24 18:13:23 +0600

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { AboutComponent } from './MyComponents/about/about.component';
+import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TodosComponent } from './MyComponents/todos/todos.component';
+
+const routes: Routes = [
+  { path: '', component: TodosComponent },
+  { path: 'about', component: AboutComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
